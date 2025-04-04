58cc75d6c94bb2e54374de08593bac9a9e64bef2
Piotr Kania <piotr@trener-it.pl>
2025-04-04 15:09:19 +0200

    Add contact form functionality

---
+++ js/contactForm.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#contact form');
+
+const formSubmit = (e) => {
+    e.preventDefault();
+}
+
+form.addEventListener('submit', formSubmit);
--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ import './scroll.js';
 import './buttons.js';
 import './modal.js';
 import './menu.js';
+import './contactForm.js';
