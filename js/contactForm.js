bef93ab7e2eebdf190b956f6017b0e80e9987412
Piotr Kania <piotr@trener-it.pl>
2025-04-04 16:03:07 +0200

    Refactor contact form structure and enhance submission handling

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,7 +1,13 @@
 const form = document.querySelector('#contact form');
 
 const formSubmit = (e) => {
+    
     e.preventDefault();
+    const inp1 = document.querySelector('input:nth-of-type(1)').value;
+    const inp2 = document.querySelector('input:nth-of-type(2)').value;
+    const inp3 = document.querySelector('textarea').value;
+
+    console.log(inp1, inp2, inp3);
 }
 
 form.addEventListener('submit', formSubmit);
